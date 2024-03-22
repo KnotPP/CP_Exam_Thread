4591a36d6be0d55412914418998a1de1b7b2e7ec
KnotPP <88419066+KnotPP@users.noreply.github.com>
2024-03-22 12:38:04 +0700

    remove lib

--- src/Example.java
+++ src/Example.java
@@ -1,17 +1,7 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 public class Example extends Application{
     @Override
